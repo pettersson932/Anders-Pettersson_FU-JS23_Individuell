0bf90153c8c7b84134d17a5b817b82ab700632c4
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-03-11 10:59:24 +0100

    header: basic

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,12 +1,14 @@
 import MainNav from "../nav/MainNav";
 import PostOrder from "../hooks/PostOrder";
 import useResponse from "../globals/Resp";
+import Header from "../header/Header";
 
 const Landing = () => {
   const { resp } = useResponse();
   return (
     <div>
       <MainNav />
+      <Header />
       <h2>Landing</h2>
     </div>
   );
