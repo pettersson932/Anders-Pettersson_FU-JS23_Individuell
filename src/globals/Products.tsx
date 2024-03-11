fecfdeb0c59b00d8655bbe7bc8090323576339f9
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-03-11 09:39:43 +0100

    menu: mapping items + increaseCart func

---
+++ src/globals/Products.tsx
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+
+const useProducts = create((set) => ({
+  items: [
+    { title: "Bryggkaffe", price: 10, count: 0 },
+    { title: "Bryggkaffe2", price: 22, count: 1 },
+    { title: "Bryggkaffe3", price: 23, count: 2 },
+  ],
+  increaseCount: (title) =>
+    set((state) => ({
+      items: state.items.map((item) =>
+        item.title === title ? { ...item, count: item.count + 1 } : item
+      ),
+    })),
+}));
+
+export default useProducts;
--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,4 +1,22 @@
+import useProducts from "../globals/Products";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  const { items, increaseCount } = useProducts();
+
+  const MenuItems = items.map((item) => (
+    <>
+      <button onClick={() => increaseCount(item.title)}> ADD </button>
+      <h4>{item.title}</h4>
+      <p>{item.price}</p>
+      <p>{item.count}</p>
+    </>
+  ));
+
+  return (
+    <div>
+      <h2>Meny</h2>
+      {MenuItems}
+    </div>
+  );
 };
 export default Menu;
