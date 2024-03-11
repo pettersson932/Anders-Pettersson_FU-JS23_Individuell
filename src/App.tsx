368060bf1d36bbfb6e6940ee295121d758dda214
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-03-11 09:24:11 +0100

    router: added + boilerplate pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Landing from "./pages/Landing";
+import Menu from "./pages/Menu";
+import Nav from "./pages/Nav";
+import Profile from "./pages/Profile";
+import Status from "./pages/Status";
+
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/nav" element={<Nav />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/status" element={<Status />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Status.tsx
@@ -0,0 +1,4 @@
+const Status = () => {
+  return <div>Status</div>;
+};
+export default Status;
