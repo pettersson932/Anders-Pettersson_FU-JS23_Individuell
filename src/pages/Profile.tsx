ffb2a4340d4f56d92f80b333b8ad6a963dd74492
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2024-03-12 22:49:43 +0100

    x

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,14 +1,43 @@
+import { useState } from "react";
 import Header from "../header/Header";
 import MainNav from "../nav/MainNav";
 import Footer from "../footer/Footer";
+import Logo from "../assets/logo/grey_small.svg";
 import "./styles/PageProfile.scss";
 
 const Profile = () => {
+  const [formData, setFormData] = useState({ name: "", email: "" });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Corrected method name
+    console.log(formData);
+  };
   return (
     <div className="pageProfile">
       <Header />
       <MainNav />
-      <h1>Profile</h1>
+      <div className="pageProfile__content">
+        <figure>
+          <img src={Logo} alt="logo" className="pageProfil__logo" />
+        </figure>
+        <h1>Välkommen till Airbean-familjen!</h1>
+
+        <form onSubmit={handleSubmit} className="pageProfile__form">
+          <label htmlFor="user__name">Name</label>
+          <input type="text" id="name" onChange={handleChange} />
+
+          <label htmlFor="user__email">Epost</label>
+          <input type="text" id="email" onChange={handleChange} />
+          <button type="submit" className="pageProfile__form__button">
+            Brew me a cup!
+          </button>
+        </form>
+      </div>
       <Footer />
     </div>
   );
